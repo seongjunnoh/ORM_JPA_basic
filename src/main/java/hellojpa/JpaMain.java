faccca50aee94261961b632dc7f6367f71544799
seongjunnoh <seongjunnoh@seongjunnohui-noteubug.local>
2024-07-05 11:09:54 +0900

    feat 단방향 연관관계 적용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,14 +16,24 @@ public class JpaMain {
 
         // code
         try {
+            // 저장
             Team team = new Team();
             team.setName("Team 1");
             em.persist(team);
 
             Member member = new Member();
-            member.setName("member1");
-            member.setTeamId(team.getId());
+            member.setName("member 1");
+            member.setTeam(team);
             em.persist(member);
+
+            // 조회
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());          // member의 PK값으로 member를 find
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam.getName() = " + findTeam.getName());
+
 
             transaction.commit();           // transaction을 commit해야 영속 컨텍스트에 있는게 db에 반영되는 것임
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -16,8 +16,20 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 
     public Long getId() {
         return id;
@@ -35,11 +47,5 @@ public class Member {
         this.name = name;
     }
 
-    public Long getTeamId() {
-        return teamId;
-    }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
-    }
 }
